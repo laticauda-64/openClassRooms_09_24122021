e36c964f763fb1d376097e7e914ded6a4f317ed7
YoannBDL <yoannb64@gmail.com>
2022-01-02 15:39:41 +0100

    [Tests unitaires et d'intégration] - Issue n°1 : views/BillsUI -> 100%
    - Composant views/Bills : faire passer le taux de couverture à 100%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,53 @@
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import {getByTestId, screen} from "@testing-library/dom";
+import {toHaveClass} from "@testing-library/jest-dom";
+import VerticalLayout from "../views/VerticalLayout";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import {ROUTES, ROUTES_PATH} from "../constants/routes";
+import BillsUI from "../views/BillsUI.js";
+import Router from "../app/Router";
+import {bills} from "../fixtures/bills.js";
+import Bills from "../containers/Bills";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
-    })
+    beforeEach(() => {
+      // Create a replica of the localStorage object, because we are not in a browser
+      Object.defineProperty(window, "localStorage", {value: localStorageMock});
+      // Ok now that we have a localStorage, we can use it to sote an user
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "yoann@bdl.com",
+          password: "azerty",
+          status: "connected",
+        })
+      );
+      // Set the current window location to the right path/adress. Like : http://127.0.0.1:8080/#employee/bills in the browser
+      Object.defineProperty(window, "location", {
+        value: {
+          hash: ROUTES_PATH["Bills"],
+        },
+      });
+    });
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      document.body.innerHTML = `<div id="root"></div>`;
+      await Router();
+      expect(screen.getByTestId("icon-window")).toHaveClass("active-icon");
+    });
+
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
-      document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      const html = BillsUI({data: bills});
+      document.body.innerHTML = html;
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
+});
